d497f518505c4dd36f10cc7c9d204434cc978d69
Mateus-IC1101 <mateus.dolglas6@gmail.com>
2023-04-13 20:12:06 -0300

    [User]

--- api/user/resolvers/userResolvers.js
+++ api/user/resolvers/userResolvers.js
@@ -1,52 +1,9 @@
-const data_users = [
-
-    {
-      "id": 2,
-      "nome": "Ana Torre",
-      "ativo": true,
-      "email": "a@a.com",
-      "role": 1
-    },
-    {
-      "id": 2,
-      "nome": "Gabriel Costa",
-      "ativo": true,
-      "email": "g@g.com",
-      "role": 1
-    },
-    {
-      "id": 3,
-      "nome": "Flavia Lins",
-      "ativo": true,
-      "email": "f@f.com",
-      "role": 1
-    },
-    {
-      "id": 4,
-      "nome": "Carlos Marques",
-      "ativo": true,
-      "email": "c@c.com",
-      "role": 2
-    },
-    {
-      "id": 5,
-      "nome": "Silvia Souza",
-      "ativo": true,
-      "email": "s@s.com",
-      "role": 2
-    },
-    {
-      "id": 6,
-      "nome": "Marcia Silva",
-      "ativo": true,
-      "email": "m@m.com",
-      "role": 2
-    }
-]
+const data_users = require('../../data/dados.json').users
 
 const userResolvers = {
     Query:{
-        users: () => data_users
+        users: () => data_users,
+        user: (id) => data_users[id]
     }
 }
 
